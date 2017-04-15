ebde2fcf51294047f4a2c3df9cfd76f80a21c2af
Alli Smith <hailalli@gmail.com>
2017-04-15 16:27:14 -0700

    get clap working

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let filenames: Vec<&str> = matches.values_of("INPUT").unwrap().collect();
+    println!("{:?}", filenames)
 }
